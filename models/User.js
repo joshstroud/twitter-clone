a197d7c0ebc880228ad189cd887b155dd61af2f5
Josh Stroud <josh.e.stroud@gmail.com>
2021-06-06 16:23:42 -0700

    Add sequelize ORM boilerplate and User model

--- models/User.js
+++ models/User.js
@@ -1,20 +1,48 @@
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
-
-const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    email: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
     },
-    password: {
-        type: String,
-        required: true
+    password_digest: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    // chats: [{
-    //     type: mongoose.ObjectId,
-    //     ref: "Chat"
-    // }]
-});
-
-UserSchema.plug(passportLocalMongoose);
-const User = mongoose.model('user', UserSchema);
+    handle: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    description: DataTypes.TEXT,
+    name: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'User',
+    indexes: [
+      {
+        unique: true,
+        fields: ['email']
+      },
+      {
+        unique: true,
+        fields: ['handle']
+      }
+    ]
+  });
+  return User;
+};
