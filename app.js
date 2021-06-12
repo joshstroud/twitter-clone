bc77e362deb20d881bd26cb564aef9d4158c8150
Josh Stroud <josh.e.stroud@gmail.com>
2021-06-12 10:06:34 -0700

    Update host port to 8080 for AWS Node port, fix bug in EB config script for Node

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
  
